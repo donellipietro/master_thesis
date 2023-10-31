988b63fa9ac9edb1a79f6afe8cf2f385641120ea
Pietro Donelli <pietro.donelli99@gmail.com>
2023-10-31 12:25:11 +0100

    Added plot utilities for fields

---
+++ utils/functions/plot_utilities.R
@@ -0,0 +1,76 @@
+# Plot utilities ----
+#||||||||||||||||||||
+
+## Settings ----
+## |||||||||||||
+
+standard_plot_settings <- function(){
+  standard_plot_settings <- theme_minimal() +
+    theme(text = element_text(size = 12),
+          plot.title = element_text(
+            color = "black",
+            face = "bold",
+            size = 14,
+            hjust = 0.5,
+            vjust = 1),
+          legend.position = "bottom",
+          legend.title = element_blank())
+}
+
+
+## Plot Fields ----
+## ||||||||||||||||
+
+# Points version
+
+plot.field_points <- function(locations, data,
+                              size = 1, limits = NULL, colormap = "D") {
+  
+  data_plot <- data.frame(locations, value = data)
+  colnames(data_plot) <- c("x", "y", "value")
+  
+  plot <- ggplot() +
+    theme_minimal() +
+    geom_point(data = data_plot, aes(x = x, y = y, color = value), size = 2.5) +
+    coord_fixed() +
+    theme(legend.position = "bottom",
+          legend.title = element_blank()) +
+    standard_plot_settings()
+  
+  if(is.null(limits)){
+    plot <- plot + scale_color_viridis(option = colormap)
+  } else {
+    plot <- plot + scale_color_viridis(option = colormap,
+                                       limits = limits)
+  }
+  
+  return(plot)
+}
+
+# Tile version
+
+plot.field_tile <- function(nodes, f,
+                            limits = NULL, colormap = "D") {
+  
+  data_plot <- data.frame(nodes, value = f)
+  colnames(data_plot) <- c("x", "y", "value")
+  
+  plot <- ggplot() +
+    theme_minimal() +
+    geom_tile(data = data_plot, aes(x = x, y = y, fill = value)) +
+    coord_fixed() +
+    standard_plot_settings()
+  
+  if(is.null(limits)){
+    plot <- plot + scale_fill_viridis(option = colormap)
+  } else {
+    plot <- plot + scale_fill_viridis(option = colormap, 
+                                      limits = limits)
+  }
+  
+  return(plot)
+}
+
+save(standard_plot_settings,
+     plot.field_points, plot.field_tile,
+     file = "utils/functions/plot_utilities.RData")
